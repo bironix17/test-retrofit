12a18b770e49e7cdc465f07da1d917bce171ce41
Dmitry Andriyash <bironix17@outlook.com>
2022-05-04 12:58:42 +0300

    add log http requests and responses

--- src/main/kotlin/ApiInterface.kt
+++ src/main/kotlin/ApiInterface.kt
@@ -1,8 +1,11 @@
 import com.example.example.CategoriesResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
 
 interface ApiInterface {
 
@@ -12,10 +15,15 @@ interface ApiInterface {
     companion object {
 
         fun create() : ApiInterface {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val httpClient = OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
 
             val retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(Constants.BASE_URL)
+                .client(httpClient.build())
                 .build()
             return retrofit.create(ApiInterface::class.java)
 
